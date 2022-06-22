e3865d5fc9cb1da4c1d98d0fb7dee1bc2f3548c4
Peter Muraus <peter.muraus@inova.si>
2022-06-22 12:39:31 +0200

    fix

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@ const action = require("../main")
 
 let tickets = action.getTickets(require("./input.json"))
 
-action.transitionTickets(["PM-1"], null, "Ready for QA", "Pero started working on this issue", "peromed.atlassian.net", "peromed@gmail.com", "JKlQWLKj2WNSeavJxZVuEE36")
+action.transitionTickets(["PM-1"], null, "Ready for QA", "Pero started working on this issue", "peromed.atlassian.net", "peromed@gmail.com", "321")
