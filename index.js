b89fc85e093f7f4448d647b110d44c962cedf619
Peter Muraus <peter.muraus@inova.si>
2022-11-28 10:30:45 +0100

    wraped in function for await

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const action = require("./main")
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-let pullRequestRef = ""
-if (github.context.payload.pull_request && github.context.payload.pull_request.head && github.context.payload.pull_request.head.ref) {
-  pullRequestRef = github.context.payload.pull_request.head.ref
+async function run() {
+  let pullRequestRef = ""
+  if (github.context.payload.pull_request && github.context.payload.pull_request.head && github.context.payload.pull_request.head.ref) {
+    pullRequestRef = github.context.payload.pull_request.head.ref
+  }
+  let before = github.context.payload.before
+  let after = github.context.payload.after
+  let commitTickets = await action.extractCommits(after, before)
+  let ticketList = [...commitTickets, pullRequestRef, github.context.payload.ref]
+  let tickets = action.getTickets(ticketList)
+  const targetTransition = core.getInput("targetTransition")
+  const sourceTransition = core.getInput("sourceTransition")
+  action.transitionTickets(
+    tickets,
+    sourceTransition,
+    targetTransition,
+    core.getInput("message"),
+    core.getInput("jiraBaseUrl"),
+    core.getInput("jiraEmail"),
+    core.getInput("jiraToken")
+  ).then(transitioned => {
+    console.log(`Tickets ${transitioned.join(", ")} transitioned to ${targetTransition}`)
+  })
 }
-let before = github.context.payload.before
-let after = github.context.payload.after
-let commitTickets = await action.extractCommits(after, before)
-let ticketList = [...commitTickets, pullRequestRef, github.context.payload.ref]
-let tickets = action.getTickets(ticketList)
-const targetTransition = core.getInput("targetTransition")
-const sourceTransition = core.getInput("sourceTransition")
-action.transitionTickets(
-  tickets,
-  sourceTransition,
-  targetTransition,
-  core.getInput("message"),
-  core.getInput("jiraBaseUrl"),
-  core.getInput("jiraEmail"),
-  core.getInput("jiraToken")
-).then(transitioned => {
-  console.log(`Tickets ${transitioned.join(", ")} transitioned to ${targetTransition}`)
-})
+run()
