b680c04574c229fc3284509e0143d1c392df30f3
Peter Muraus <peter.muraus@inova.si>
2023-01-18 11:57:59 +0100

    added logs for ref for testing

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function run() {
   if (github.context.payload.pull_request && github.context.payload.pull_request.head && github.context.payload.pull_request.head.ref) {
     pullRequestRef = github.context.payload.pull_request.head.ref
   }
+  console.log(github.context.payload)
   const branch = github.context.payload.ref.split('/').pop()
   let after = github.context.payload.after
   const workflowName = process.env["GITHUB_WORKFLOW"]
